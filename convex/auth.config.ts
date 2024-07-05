5ca84ac1cb8e2ab4ddb16c4ac941f5e22660f05f
Evan Honggo Widjojo <evanedreo@gmail.com>
2024-07-05 03:21:07 -0700

    finished the components, auth, layout

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://pure-seahorse-48.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig
